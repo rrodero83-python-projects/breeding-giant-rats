26968b13fc2eea97bb3f9d3dc872622885438ae4
rrodero83 <rafaelhenrique.rodero@gmail.com>
2020-01-26 21:05:19 -0300

    Challenge Project - Rat Harem

--- super_rats_harem.py
+++ super_rats_harem.py
@@ -1,13 +1,18 @@
 import time
 import random
 import statistics
+import itertools
 
 # CONSTANTS (weights in grams)
 GOAL = 50000
 NUM_RATS = 20
+NUM_MALE_RATS = 4
+NUM_FEMALE_RATS = 16
 INITIAL_MIN_WT = 200
 INITIAL_MAX_WT = 600
+INITIAL_MODE_FEMALE_WT = 250
 INITIAL_MODE_WT = 300
+INITIAL_MODE_MALE_WT = 300
 MUTATE_ODDS = 0.01
 MUTATE_MIN = 0.5
 MUTATE_MAX = 1.2
@@ -31,15 +36,16 @@ def fitness(population, goal):
     return ave / goal
 
 
-def select(population, to_retain):
+def select(population, males_to_retain, females_to_retain):
     """Cull a population to retain only a specified number of members."""
     sorted_population = sorted(population)
-    to_retain_by_sex = to_retain // 2
-    members_per_sex = len(sorted_population) // 2
-    females = sorted_population[:members_per_sex]
-    males = sorted_population[members_per_sex:]
-    selected_females = females[-to_retain_by_sex:]
-    selected_males = males[-to_retain_by_sex:]
+    # to_retain_by_sex = to_retain // males_to_retain
+    male_members = int(len(sorted_population) * (males_to_retain / females_to_retain))
+    female_members = len(sorted_population) - male_members
+    females = sorted_population[:female_members]
+    males = sorted_population[-male_members:]
+    selected_females = females[-females_to_retain:]
+    selected_males = males[-males_to_retain:]
     return selected_males, selected_females
 
 
@@ -48,9 +54,12 @@ def breed(males, females, litter_size):
     random.shuffle(males)
     random.shuffle(females)
     children = []
-    for male, female in zip(males, females):
+    for male, female in itertools.zip_longest(males, females, fillvalue=random.choice(males)):
         for child in range(litter_size):
-            child = random.randint(female, male)
+            if female > male:
+                child = random.randint(male, female)
+            else:
+                child = random.randint(female, male)
             children.append(child)
 
     return children
@@ -68,8 +77,11 @@ def mutate(children, mutate_odds, mutate_min, mutate_max):
 def main():
     """Initialize population, select, breed, and mutate, display results."""
     generations = 0
+    # male_parents = populate(NUM_MALE_RATS, INITIAL_MIN_WT, INITIAL_MAX_WT, INITIAL_MODE_MALE_WT)
+    # female_parents = populate(NUM_FEMALE_RATS, INITIAL_MIN_WT, INITIAL_MAX_WT, INITIAL_MODE_FEMALE_WT)
     parents = populate(NUM_RATS, INITIAL_MIN_WT, INITIAL_MAX_WT, INITIAL_MODE_WT)
     print("initial population weights = {}".format(parents))
+    # print("initial female population weights = {}".format(female_parents))
     popl_fitness = fitness(parents, GOAL)
     print("initial population fitness = {}".format(popl_fitness))
     print("number to retain = {}".format(NUM_RATS))
@@ -77,7 +89,7 @@ def main():
     ave_wt = []
 
     while popl_fitness < 1 and generations < GENERATION_LIMIT:
-        selected_males, selected_females = select(parents, NUM_RATS)
+        selected_males, selected_females = select(parents, NUM_MALE_RATS, NUM_FEMALE_RATS)
         children = breed(selected_males, selected_females, LITTER_SIZE)
         children = mutate(children, MUTATE_ODDS, MUTATE_MIN, MUTATE_MAX)
         parents = selected_males + selected_females + children
